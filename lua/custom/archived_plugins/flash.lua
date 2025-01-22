0437ff610c6abad0edf091c51b996b74d9f2ebb3
Travis <travis@mooremediallc.com>
2025-01-22 05:52:55 -0700

    updated

---
+++ lua/custom/archived_plugins/flash.lua
@@ -0,0 +1,13 @@
+-- Not using this plugin because this plugin breaks when trying to search for words and the same key pops up as the word you are searching for.
+-- return {
+--   {
+--     'folke/flash.nvim',
+--     opts = {
+--       modes = {
+--         search = {
+--           enabled = true,
+--         },
+--       },
+--     },
+--   },
+-- }
